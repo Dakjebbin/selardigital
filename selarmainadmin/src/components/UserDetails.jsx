8ced83510939e3539a0454a858f7a11d7008d8a3
DakJebbin <Jebbinp@gmail.com>
2025-04-26 22:16:06 +0100

    recovered lost files/ created new folder

---
+++ selarmainadmin/src/components/UserDetails.jsx
@@ -0,0 +1,509 @@
+import { useEffect, useRef, useState } from "react";
+import { assets } from "../assets/assest";
+import { useAuthContext } from "../context/auth-context";
+import { MdDashboard } from "react-icons/md";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import toast from "react-hot-toast";
+import { FaSpinner } from "react-icons/fa";
+import axios from "axios";
+import { MdOutlineMenuOpen } from "react-icons/md";
+import { IoIosLogOut } from "react-icons/io";
+import { RxAvatar } from "react-icons/rx";
+import { useParams } from "react-router-dom";
+
+const UserDetails = () => {
+ 
+  const { userData } = useAuthContext();
+  const [open, setOpen] = useState(true);
+  const [loggingOut, setLoggingOut] = useState(false);
+  const [loading, setLoading] = useState(true);
+  // const [updated, setUpdated] = useState(false);
+  const { Id } = useParams();
+  const [userDetails, setUserDetails] = useState(null);
+  const [isApproved, setIsApproved] = useState("Approved")
+  // const [status, setStatus] = useState("active")
+  // const [signalAvailable, setSignalAvailable] = useState("Signal")
+  const [transaction, setTransaction] = useState([])
+
+  const baseUrl = import.meta.env.VITE_BASE_URL;
+
+  axios.defaults.withCredentials = true;
+
+  const handleSelectChange =(e) => {
+    setIsApproved(e.target.value)
+  }
+  
+  const handleApprovedStatus = async () => {
+    try {
+        const response = await axios.patch(`${baseUrl}/auth/approvedStatus/${Id}`, 
+            { isApproved },
+            {
+            withCredentials: true,
+        })
+        
+        if (response.status === 201) {
+          toast.success("User's Approval status has been updated");
+          
+        }
+
+        setUserDetails((prevDetails) => ({
+          ...prevDetails,
+          isApproved: isApproved, // Update only the approval status
+        }));
+
+    } catch (error) {
+        if (error instanceof axios.AxiosError) {
+            toast.error(error?.response?.data);
+        } else {
+            toast.error("Error fetching users: ", error.message);
+        }
+    }     
+  }
+
+  // const handleStatusUpdate = async () => {
+    
+  //   try {
+  //     const response = await axios.patch(`${baseUrl}/auth/status/${Id}`,{
+  //       status
+  //     },{
+  //       withCredentials: true,
+  //     })
+
+  //     if (response.status === 201) {
+  //       toast.success("User's status has been updated");
+        
+  //     }
+      
+  //     setUserDetails((prevStatus) => ({
+  //       ...prevStatus,
+  //       status: status, // Update only the status
+  //     }));
+  //   } catch (error) {
+  //     if (error instanceof axios.AxiosError) {
+  //       toast.error(error?.response?.data);
+  //   } else {
+  //       toast.error("Error fetching users: ", error.message);
+  //   }
+  //   }
+  // }
+
+  // const handleSignalUpdate = async () => {
+   
+    
+  //   try {
+  //     setUpdated(true);
+  //     const response = await axios.patch(`${baseUrl}/auth/signal/${Id}`,{
+  //       signalAvailable
+  //     },{
+  //       withCredentials: true,
+  //     })
+
+  //     if (response.status === 200) {
+  //       toast.success("User's Signal Status has been updated");
+
+  //     }
+      
+  //     setUserDetails((prevStatus) => ({
+  //       ...prevStatus,
+  //       signalAvailable, // Update only the status
+  //     }));
+  //   } catch (error) {
+  //     if (error instanceof axios.AxiosError) {
+  //       toast.error(error?.response?.data);
+  //   } else {
+  //       toast.error("Error fetching users: ", error.message);
+  //   }
+  //   } finally {
+  //     setUpdated(false);
+  //   }
+  // }
+
+  useEffect(() => {
+  const fetchTransaction = async () => {
+      try {
+        const response = await axios.get(`${baseUrl}/transactions/get-transactionAdmin/${Id}`,{
+          withCredentials:true
+        })
+
+        setTransaction(response.data.data)
+        
+      } catch (error) {
+        if (error instanceof axios.AxiosError) {
+          toast.error(error?.response?.data?.message || "Something went wrong" );
+      } else {
+          toast.error("Error");
+      }
+      }
+  }
+
+  fetchTransaction();
+  
+  },[Id])
+
+
+
+    const fetchUserDetails = async () => {
+        try {
+            const response = await axios.get(`${baseUrl}/auth/users/${Id}`, { withCredentials: true });
+            setUserDetails(response.data.user);
+            setLoading(false);
+        } catch (error) {
+            if (error instanceof axios.AxiosError) {
+                toast.error(error?.response?.data?.message || "Something went wrong" );
+            } else {
+                toast.error("Error fetching users: ", error.message);
+            }
+        }
+    }; 
+
+    useEffect(() => {
+        fetchUserDetails();
+    },
+ [Id, baseUrl]);
+
+  useEffect(() => {
+    // Ensure that userData exists and we can safely check for isAdmin
+    if (userData !== null) {
+        if (userData.isAdmin !== "ADMIN") {
+            toast.error("Unauthorized Access");
+            window.location.assign("/");
+        } else {
+            // If the user is an admin, stop loading
+            setLoading(false);
+        }
+    }
+}, [userData]); 
+
+
+  const handleLogout = async (e) => {
+    e.preventDefault();
+
+    setLoggingOut(true);
+    try {
+      const response = await axios.post(
+        `${baseUrl}/auth/logout`,
+        {
+          withCredentials: true,
+        }
+      );
+
+      if (response?.data.success) {
+        toast.success(response?.data?.message);
+        window.location.assign("/");
+      }
+    } catch (error) {
+      if (error instanceof axios.AxiosError) {
+        toast.error("");
+      }
+      if (error === 404 || error) {
+        const errorMessage = error.message;
+        toast.error(errorMessage);
+      }
+    } finally{
+      setLoggingOut(false);
+    }
+  };
+
+  const menuitems = [
+    {
+      icons: <MdDashboard size={30} />,
+      label: "Dashboard",
+      url: "/admin-dashboard",
+    },
+    
+    
+  ];
+
+  const [isNavActive, setIsNavActive] = useState(false);
+  const mobileNavRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (mobileNavRef.current && !mobileNavRef.current.contains(event.target)) {
+        setIsNavActive(false);
+      }
+    };
+
+    // Add event listener
+    document.addEventListener("mousedown", handleClickOutside);
+
+    // Clean up the event listener on component unmount
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  const handleToggle = () => {
+    setIsNavActive(!isNavActive);
+  };
+  
+  if (loading) {
+    return <div>Loading User details...</div>;
+}
+
+if (!userDetails) {
+    return <div>User not found!</div>;
+}
+
+const formatDate = (timestamp) => {
+  const date = new Date(timestamp);
+  return date.toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+  });
+};
+  return (
+    <div className="md:flex">
+      {userData && (
+        <>
+          {/* Sidebar for Desktop */}
+          <nav
+            className={`shadow-md p-2 bg-[#A69051] hidden flex-col ${open ? `w-60` : `w-16`} md:flex duration-500 sticky top-0 h-screen`}
+          >
+            {/* Header */}
+            <div className="px-3 py-2 h-20 flex justify-between items-center">
+              <img src={assets.logo} alt="logo" className={`${open ? `w-10` : `w-0`} rounded-md`} />
+              <div>
+                <MdOutlineMenuOpen
+                  size={34}
+                  className={`duration-500 cursor-pointer ${!open && `rotate-180`}`}
+                  onClick={() => setOpen(!open)}
+                />
+              </div>
+            </div>
+
+            {/* Body */}
+            <ul className="flex-1">
+              {menuitems.map((item, index) => (
+                <li key={index} className="px-1 py-2 my-2 relative duration-300 flex gap-2 items-center group">
+                  <a className=" hover:bg-white rounded-md cursor-pointer pt-1 pl-2 pr-32" href={item.url}>
+                    <div>{item.icons}</div>
+                    <p className={`${!open && `w-0 translate-x-24`} duration-500 overflow-hidden`}>
+                      {item.label}
+                    </p>
+                  </a>
+                  <p
+                    className={`${open && "hidden"} absolute left-120 shadow-md rounded-md w-0 p-0 duration-300 overflow-hidden group-hover:w-fit group-hover:p-2 group-hover:left-16`}
+                  >
+                    {item.label}
+                  </p>
+                </li>
+              ))}
+            </ul>
+
+            {/* Footer */}
+            <div className="flex items-center gap-2 px-3 py-2">
+              <div>
+                 <Avatar>
+                  <AvatarImage src="https://github.com/shadcn.png" />
+                  <AvatarFallback>WW</AvatarFallback>
+                </Avatar> 
+              </div>
+              <div className={`leading-5 ${!open && `w-0 translate-x-24`} duration-500 overflow-hidden`}>
+                <p className="flex items-center mr-3">{userData?.username}</p>
+                <p className="text-xs uppercase">{userData?.email}</p>
+
+                
+                <button 
+                onClick={handleLogout} 
+                className="flex items-center bg-red-600 rounded-md text-white p-1"
+                disabled={loggingOut}
+                >
+                  
+                  {loggingOut ? (
+                    <div className="flex items-center space-x-2 justify-center">
+                    <span className="animate-pulse">Logging Out</span>{" "}
+                    <FaSpinner className=" animate-spin " />
+                  </div>
+                  ) : (
+                    <div className="flex">
+                  <p className="mr-1 font-bold">Logout</p>
+                  <IoIosLogOut className="cursor-pointer" size={25} />
+                  </div>
+                )}
+                </button>
+
+              </div>
+            </div>
+          </nav>
+
+          {/* Sidebar for Mobile */}
+          <div className="md:hidden">
+            <div className="m-6 cursor-pointer" onClick={handleToggle} ref={mobileNavRef}>
+              <MdOutlineMenuOpen size={34} />
+            </div>
+
+            <div
+              ref={mobileNavRef}
+              className={`fixed top-0 right-0 w-64 h-full bg-[#FFBBB8] transform transition-all duration-500 ${isNavActive ? "translate-x-0" : "translate-x-full"}`}
+            >
+              <div className="ml-5 mt-4">
+                <img src={assets.logo} alt="logo" className="w-14 rounded-md" />
+              </div>
+
+              <nav>
+                <ul>
+                  {menuitems.map((item, index) => (
+                    <li key={index} className="p-5 hover:bg-white hover:rounded-md hover:m-2 cursor-pointer">
+                      <a href={item.url}>
+                        <div>{item.icons}</div>
+                        <p>{item.label}</p>
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+
+                <div className="flex items-center gap-2 pt-4 pl-4">
+                  <div>
+                     <Avatar>
+                      <AvatarImage src="https://github.com/shadcn.png" />
+                      <AvatarFallback>WW</AvatarFallback>
+                    </Avatar> 
+                  </div>
+                  <div className="leading-5">
+                    <p className="flex items-center mr-3">{userData?.username}</p>
+                    <p className="text-xs uppercase">{userData?.email}</p>
+
+ 
+                    <button 
+                onClick={handleLogout} 
+                className="flex items-center bg-red-600 rounded-md text-white p-1"
+                disabled={loggingOut}
+                >
+                  
+                  {loggingOut ? (
+                    <div className="flex items-center space-x-2 justify-center">
+                    <span className="animate-pulse">Logging Out</span>{" "}
+                    <FaSpinner className=" animate-spin " />
+                  </div>
+                  ) : (
+                    <div className="flex">
+                  <p className="mr-1 font-bold">Logout</p>
+                  <IoIosLogOut className="cursor-pointer" size={25} />
+                  </div>
+                )}
+                </button>
+                  </div>
+                </div>
+              </nav>
+            </div>
+          </div>
+
+          {/* Content area */}
+          <div
+            className={`flex-1 p-5 overflow-auto  md:max-h-screen transition-all duration-500 ${open ? "ml-4" : "ml-5"}`}
+          >
+        <div>
+            <div className="flex items-center justify-center m-10">
+            <RxAvatar size={69} />
+            </div>
+
+            <div className="flex flex-col gap-4 font-semibold text-xl">
+                <p>Full Name: - {userDetails.fullname}</p>
+                <p>UserName: - {userDetails.username}</p>
+                <p>Email: - {userDetails.email}</p>
+                <p>Phone Number: - {userDetails.phonenumber}</p>
+                {/* <p>Status: - {userDetails.status}</p> */}
+                <p>IsApproved: - {userDetails.isApproved}</p>
+                {/* I'd correct you later */}
+                <p>Balance: - ${userDetails.balance}</p>
+                <p>Profit: - ${userDetails.profit}</p>
+                {/* <p>Is Signal Available:- {userDetails.signalAvailable}</p> */}
+            </div>
+
+            
+            <div className="flex-col md:flex-row flex justify-between mx-2 sm:mx-6 mt-9 ">
+           
+                <div className="flex flex-col ">
+                <label>Approve User</label>
+                <select name="" value={isApproved}
+                onChange={handleSelectChange}
+                id=""
+                 className="border sm:px-4 py-2 rounded-md font-semibold text-xl">
+                    <option value="Approved" className="font-semibold" >Approved</option>
+                    <option value="Not Approved">Not Approved</option>
+                </select>
+
+                <div className="mb-4">
+                    <button onClick={handleApprovedStatus} className="bg-[#14AE5C] px-5 py-2 mt-5 
+                    font-playfair font-semibold rounded-lg">Update</button>
+                </div>
+                </div>
+             
+                {/* <div className="flex flex-col">
+                <label>Update User Status</label>
+                <select value={status}
+                 id=""
+                 onChange={(e) => setStatus(e.target.value)}
+                 className="border px-4 py-2  rounded-md font-semibold text-xl">
+                    <option value="active" className="font-semibold" >Active</option>
+                    <option value="blocked">Blocked</option>
+                </select>
+
+                <div className="mb-4">
+                    <button onClick={handleStatusUpdate} className="bg-[#14AE5C] px-5 py-2 mt-5 font-playfair font-semibold rounded-lg">Update</button>
+                </div>
+                </div> */}
+  
+  {/* Signal Update on the website */}
+                {/* <div className="flex flex-col">
+                <label>Signal Status</label>
+                <select value={signalAvailable}
+                 id=""
+                 onChange={(e) => setSignalAvailable(e.target.value)}
+                 className="border px-4 py-2  rounded-md font-semibold text-xl">
+                    <option value="Signal" className="font-semibold" >Signal</option>
+                    <option value="No Signal">No Signal</option>
+                </select>
+
+                <div className="mb-4">
+                    <button onClick={handleSignalUpdate} className="bg-[#14AE5C] px-5 py-2 mt-5 font-playfair font-semibold rounded-lg">{
+                      updated ? "Updating" : "Update"
+                      }</button>
+                </div>
+                </div> */}
+           
+           
+            </div>
+        </div>
+
+        <div className="mt-14">
+          <p className="text-center font-playfair text-xl font-semibold">Transaction History</p>
+
+          <div className="bg-[#FFE6E4] mt-9">
+            <ol className="flex justify-between mx-2 sm:mx-9 font-playfair">
+              <li>Date</li>
+              <li>Status</li>
+              <li>Amount</li>
+              <li>Type</li>
+            </ol>
+          </div>
+
+          <div>
+            {transaction.length === 0 ? (
+              <p>No Transaction Available</p>
+            ) : (
+              <div>
+                {transaction.map((transactions, index) => (
+                  <div key={index} className="flex justify-between mt-3 mx-2 sm:mx-9 font-playfair text-base sm:text-xl">
+                      <p className="mb-3"> {formatDate(transactions.createdAt)}</p>
+                      <p>{transactions.status}</p>
+                      <p>${transactions.amount}</p>
+                      <p>{transactions.type}</p>
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
+        </div>
+          </div>
+        </>
+      )}
+   
+    </div>
+   
+  );
+};
+
+export default UserDetails;
---
+++ selarmainadmin/src/main.jsx
@@ -0,0 +1,40 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import  { Toaster } from 'react-hot-toast';
+import './index.css'
+import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router';
+import AuthContextProvider from './context/auth-context.jsx';
+import ForgotPassowrd from './Pages/ForgotPassword.jsx';
+import SidebarCourses from "./components/Sidebar.jsx"
+import UserDetails from './components/UserDetails.jsx';
+import FundUser from './components/FundUser.jsx';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>
+  },{
+    path: "/forgotPassword",
+    element: <ForgotPassowrd/>
+  },{
+    path: "/admin-dashboard",
+    element: <SidebarCourses/>
+  },{
+    path:"/admin-dashboard/user/:Id",
+    element: <UserDetails/>
+  },{
+    path: "/admin-dashboard/Fund/:Id",
+    element: <FundUser/>
+  }
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+     <AuthContextProvider>
+    <RouterProvider router={router}/>
+    <Toaster />
+    </AuthContextProvider>
+  </StrictMode>,
+)
