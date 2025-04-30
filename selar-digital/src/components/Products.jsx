226da85e1b5d2a3f24bdd393294657ee1b1af058
DakJebbin <Jebbinp@gmail.com>
2025-04-30 09:32:40 +0100

    recent updates

--- selar-digital/src/components/Products.jsx
+++ selar-digital/src/components/Products.jsx
@@ -20,9 +20,9 @@ const Products = () => {
           whileInView={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.6, ease: "easeOut" }}
           viewport={{  amount: 0.2 }}
-          className="grow basis-[200px] bg-[#A69051] px-5 py-5 rounded-tr-3xl rounded-bl-3xl"
+          className="grow basis-[200px] flex flex-col justify-between bg-[#A69051] px-5 py-5 rounded-tr-3xl rounded-bl-3xl"
         >
-          <Link to="/login" onClick={() => scrollX(0, 0)}>
+          
             <div>
               <img className="rounded-md" loading="lazy" src={assets.course_1} alt="" />
             </div>
@@ -36,21 +36,27 @@ const Products = () => {
               </p>
               <p className="text-lg underline mb-2">Basic</p>
 
-              <div className="flex items-center">
-               <img className="mr-4" src={assets.books} alt="" /> 
-                <p className="text-lg text-center font-bold">$500 - $10,000</p>
+              <div className="flex items-center justify-center my-4">
+              <Link to="/login" onClick={() => scrollX(0, 0)}>
+                <button className="bg-black text-white cursor-pointer px-3 py-2 text-lg rounded-2xl">Purchase</button>
+                </Link>
               </div>
 
-              <div className="flex items-center mt-1">
-                {/* <img className="mr-4" src={assets.books} alt="" /> */}
+              {/* <div className="flex items-center">
+               <img className="mr-4" src={assets.books} alt="" /> 
+                <p className="text-lg text-center font-bold">$500 - $10,000</p>
+              </div> */}
+
+              {/* <div className="flex items-center mt-1">
+                <img className="mr-4" src={assets.books} alt="" />
                 <div>
                 <p className="text-lg">Min Profit - 5%</p>
                 <p className="text-lg">Max Profit - 5000%</p>
                 <p className="text-lg">Gift Bonus - $100</p>
                 </div>
-              </div>
+              </div> */}
             </div>
-          </Link>
+        
         </motion.div>
 
         <motion.div
@@ -58,14 +64,14 @@ const Products = () => {
           whileInView={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.8, ease: "easeOut" }}
           viewport={{ amount: 0.2 }}
-          className="grow basis-[200px] bg-[#FCEFCB] px-5 py-5 rounded-tl-3xl rounded-br-3xl"
+          className="grow basis-[200px] flex flex-col justify-between bg-[#FCEFCB] px-5 py-5 rounded-tl-3xl rounded-br-3xl"
         >
-          <Link to="/login" onClick={() => scrollX(0, 0)}>
+       
             <img className="rounded-md" loading="lazy" src={assets.course_2} alt="" />
-          </Link>
+        
 
           <div>
-            <Link to="/login" onClick={() => scrollX(0, 0)}>
+         
               <p className="font-bold text-lg text-center my-3">
                 Affiliate Lab
               </p>
@@ -74,20 +80,28 @@ const Products = () => {
               </p>
               <p className="text-lg underline mb-2">Standard</p>
 
-              <div className="flex items-center">
+              
+              <div className="flex items-center justify-center my-4">
+              <Link to="/login" onClick={() => scrollX(0, 0)}>
+                <button className="bg-black text-white cursor-pointer px-3 py-2 text-lg rounded-2xl">Purchase</button>
+                </Link>
+              </div>
+              
+
+              {/* <div className="flex items-center">
                 <img className="mr-4" src={assets.books} alt="" />
                 <p className="text-lg text-center font-bold">$10,000 - $50,000</p>
               </div>
 
               <div className="flex items-center mt-1">
-                {/* <img className="mr-4" src={assets.books} alt="" /> */}
+               <img className="mr-4" src={assets.books} alt="" />
                 <div>
                 <p className="text-lg">Min Profit - 10%</p>
                 <p className="text-lg">Max Profit - 5000%</p>
                 <p className="text-lg">Gift Bonus - $250</p>
                 </div>
-              </div>
-            </Link>
+              </div> */}
+           
           </div>
         </motion.div>
 
@@ -96,14 +110,14 @@ const Products = () => {
           whileInView={{ opacity: 1, y: 0 }}
           transition={{ duration: 1, ease: "easeOut" }}
           viewport={{  amount: 0.2 }}
-          className="grow basis-[200px] bg-[#FCEFCB] px-5 py-5 rounded-tr-3xl rounded-bl-3xl"
+          className="grow basis-[200px] flex flex-col justify-between bg-[#FCEFCB] px-5 py-5 rounded-tr-3xl rounded-bl-3xl"
         >
-          <Link to="/login" onClick={() => scrollX(0, 0)}>
+         
             <img className="rounded-md" loading="lazy" src={assets.course_3} alt="" />
-          </Link>
+         
 
           <div>
-            <Link to="/login" onClick={() => scrollX(0, 0)}>
+         
               <p className="font-bold text-lg text-center my-3">
                 HubSpot Academy
               </p>
@@ -112,20 +126,25 @@ const Products = () => {
               </p>
               <p className="text-lg underline mb-2">Premium</p>
 
-              <div className="flex items-center">
+              <div className="flex items-center justify-center my-4">
+              <Link to="/login" onClick={() => scrollX(0, 0)}>
+                <button className="bg-black text-white cursor-pointer px-3 py-2 text-lg rounded-2xl">Purchase</button>
+                </Link>
+              </div>
+
+              {/* <div className="flex items-center">
                 <img className="mr-4" src={assets.books} alt="" />
                 <p className="text-lg text-center font-bold">$50,000 - $500,000</p>
               </div>
 
               <div className="flex items-center mt-1">
-                {/* <img className="mr-4" src={assets.books} alt="" /> */}
+                 <img className="mr-4" src={assets.books} alt="" />
                 <div>
                 <p className="text-lg">Min Profit - 15%</p>
                 <p className="text-lg">Max Profit - 5000%</p>
                 <p className="text-lg">Gift Bonus - $1,000</p>
                 </div>
-              </div>
-            </Link>
+              </div> */}
           </div>
         </motion.div>
 
@@ -134,14 +153,14 @@ const Products = () => {
           whileInView={{ opacity: 1, y: 0 }}
           transition={{ duration: 1.2, ease: "easeOut" }}
           viewport={{  amount: 0.2 }}
-          className="grow basis-[200px] bg-[#A69051] px-5 py-5 rounded-tl-3xl rounded-br-3xl"
+          className="grow flex flex-col justify-between basis-[200px] bg-[#A69051] px-5 py-5 rounded-tl-3xl rounded-br-3xl"
         >
-          <Link to="/login" onClick={() => scrollX(0, 0)}>
+     
             <img className="rounded-md" loading="lazy" src={assets.course_4} alt="" />
-          </Link>
+     
 
           <div className=" text-white">
-            <Link to="/login" onClick={() => scrollX(0, 0)}>
+   
               <p className="font-bold text-lg text-center my-3">
                 Savage Affiliates
               </p>
@@ -150,20 +169,26 @@ const Products = () => {
               </p>
               <p className="text-lg underline mb-2">Legendary</p>
 
+              <div className="flex items-center justify-center my-4">
+              <Link to="/login" onClick={() => scrollX(0, 0)}>
+                <button className="bg-black text-white cursor-pointer px-3 py-2 text-lg rounded-2xl">Purchase</button>
+                </Link>
+              </div>
+{/* 
               <div className="flex items-center">
                 <img className="mr-4" src={assets.books} alt="" />
                 <p className="text-lg text-center font-bold">$500,000 - $5,000,000</p>
               </div>
 
               <div className="flex items-center mt-1">
-                {/* <img className="mr-4" src={assets.books} alt="" /> */}
+                 <img className="mr-4" src={assets.books} alt="" /> 
                 <div>
                 <p className="text-lg">Min Profit - 20%</p>
                 <p className="text-lg">Max Profit - 5000%</p>
                 <p className="text-lg">Gift Bonus - $5,000</p>
                 </div>
-              </div>
-            </Link>
+              </div> */}
+        
           </div>
         </motion.div>
       </section>
