b21ba950fd64feb29cb04522dc2d64a111e31b07
Dakjebbin <jebbinp@gmail.com>
2025-06-20 00:05:25 +0100

    done

--- selar-digital/src/components/PinModal.jsx
+++ selar-digital/src/components/PinModal.jsx
@@ -45,7 +45,7 @@ const PinModal = ({ setShowModal, onPinVerified }) => {
         className="bg-white p-6 rounded-lg shadow-lg w-80"
       >
         <button
-          className="absolute top-2 right-2 text-xl text-gray-500"
+          className=" text-xl text-gray-500"
           onClick={() => setShowModal(false)} // Close modal manually on close button click
         >
           &times;
